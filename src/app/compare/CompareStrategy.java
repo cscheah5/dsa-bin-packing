61e55e946f960158f5cc7f9e05ecb63cce639dcb
J1nnHAlt <jinhaowngaltacc@gmail.com>
2025-04-24 22:25:17 +0800

    ....

--- src/app/compare/CompareStrategy.java
+++ src/app/compare/CompareStrategy.java
@@ -1,22 +1,27 @@
 package app.compare;
+import java.util.*;
 
+import app.model.Truck;
 import app.model.TruckLoadingProblem;
 import app.strategy.TruckLoadingStrategy;
 
 public class CompareStrategy {
 	// TODO
-	public static void compareTimeComplexity(TruckLoadingStrategy strat1, TruckLoadingStrategy strat2, TruckLoadingProblem problem) {
-
+	public static void compareTimeComplexity(TruckLoadingStrategy firstFit, TruckLoadingStrategy bestFit, TruckLoadingProblem problem) {
+		List<Truck> firstFitTruck = firstFit.solve(problem);
+		List<Truck> bestFitTruck = bestFit.solve(problem);
 	}
 
 	// TODO
-	public static void compareTime(TruckLoadingStrategy strat1, TruckLoadingStrategy strat2, TruckLoadingProblem problem) {
-
+	public static void compareTime(TruckLoadingStrategy firstFit, TruckLoadingStrategy bestFit, TruckLoadingProblem problem) {
+		List<Truck> firstFitTruck = firstFit.solve(problem);
+		List<Truck> bestFitTruck = bestFit.solve(problem);
 	}
 
 	// TODO
-	public static void compareMemory(TruckLoadingStrategy strat1, TruckLoadingStrategy strat2, TruckLoadingProblem problem) {
-
+	public static void compareMemory(TruckLoadingStrategy firstFit, TruckLoadingStrategy bestFit, TruckLoadingProblem problem) {
+		List<Truck> firstFitTruck = firstFit.solve(problem);
+		List<Truck> bestFitTruck = bestFit.solve(problem);
 	}
 
 }
